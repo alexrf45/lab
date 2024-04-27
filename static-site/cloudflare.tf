9a57cd14138efaa8c7e17db240667ba79fe25eb9
alexrf45 <fonalex45@gmail.com>
2024-04-27 12:51:41 -0400

    testing

--- static-site/cloudflare.tf
+++ static-site/cloudflare.tf
@@ -23,7 +23,7 @@ resource "cloudflare_record" "acm" {
 
 resource "cloudflare_record" "cname" {
   depends_on      = [aws_cloudfront_distribution.dist]
-  zone_id         = data.cloudflare_zones.domain.id
+  zone_id         = data.cloudflare_zones.domain[0].id
   name            = var.site_domain
   value           = aws_cloudfront_distribution.dist.domain_name
   type            = "CNAME"
