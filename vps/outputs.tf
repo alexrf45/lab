00086bd5f541cb18bc885a03b54cfb1bfad00c0f
alexrf45 <fonalex45@gmail.com>
2024-05-11 13:20:39 -0400

    output fix

--- vps/outputs.tf
+++ vps/outputs.tf
@@ -1,10 +1,10 @@
 output "id" {
   description = "The ID of the instance"
-  value       = aws_instance.jump_instance.id
+  value       = aws_instance.instance.id
 }
 
 output "vps_public_ip" {
   description = "public ip from Elastic IP allocation"
-  value       = aws_eip.jump_eip.public_ip
+  value       = aws_eip.eip.public_ip
 }
 
