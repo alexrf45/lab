3f344ed55e4c10426f607e3d1a13be248392d282
alexrf45 <fonalex45@gmail.com>
2024-06-03 22:51:11 -0400

    more

--- static-site/terraform.tf
+++ static-site/terraform.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.46.0"
+      source                = "hashicorp/aws"
+      version               = "5.46.0"
+      configuration_aliases = [aws.west, aws.east]
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
