c3e635d3bd5e48cdd1faabbe3fd8282fa06f3bee
alexrf45 <fonalex45@gmail.com>
2025-01-23 08:59:52 -0500

    output to child module

--- talos-pve-flux/talos.tf
+++ talos-pve-flux/talos.tf
@@ -126,6 +126,12 @@ resource "local_sensitive_file" "kubeconfig" {
   file_permission = "0600"
 }
 
+resource "local_sensitive_file" "kubeconfig_2" {
+  content         = talos_cluster_kubeconfig.this.kubeconfig_raw
+  filename        = "${path.module}/outputs/kubeconfig"
+  file_permission = "0600"
+}
+
 resource "local_sensitive_file" "talosconfig" {
   content  = data.talos_client_configuration.this.talos_config
   filename = "${path.root}/outputs/talosconfig"
