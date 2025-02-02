60c4c571e221ab51a10c4123925f27b5dc504f4d
fr3d <76225810+alexrf45@users.noreply.github.com>
2025-02-02 14:00:57 -0500

    label format (#37)

--- talos-pve/talos.tf
+++ talos-pve/talos.tf
@@ -35,7 +35,7 @@ data "talos_machine_configuration" "this" {
       hostname      = format("%s-controlplane-%s", var.cluster.name, index(keys(var.nodes), each.key))
       node_name     = each.value.node
       cluster_name  = var.cluster.name
-      node_label    = "node-role.kubernetes.io/control-plane=true"
+      node_label    = "node-role.kubernetes.io/control-plane"
 
     }),
     yamlencode({
@@ -60,7 +60,7 @@ data "talos_machine_configuration" "this" {
       hostname      = format("%s-node-%s", var.cluster.name, index(keys(var.nodes), each.key))
       node_name     = each.value.node
       cluster_name  = var.cluster.name
-      node_label    = "node-role.kubernetes.io/worker=true"
+      node_label    = "node-role.kubernetes.io/worker"
     })
   ]
 }
